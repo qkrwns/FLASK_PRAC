26c3c7d48d94abc37b932b01bd6d28dc54e86897
qkrwns <jpthreesera@naver.com>
2024-08-17 11:12:20 +0900

    asdf

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-asdf
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/', methods = ('GET', 'POST')) # 접속하는 url
+def index():
+  return render_template('index.html',user="반원",data={'level':60,'point':360,'exp':45000})
+
+if __name__=="__main__":
+  app.run(debug=True)
+
+
+
